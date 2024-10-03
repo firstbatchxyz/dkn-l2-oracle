d912723346c31fd1f54ced275b97c900f3ba6c6d
erhant <erhany96@gmail.com>
2024-10-03 23:52:49 +0300

    smol fix

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use eyre::{Context, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    env_logger::try_init().wrap_err("could not initialize env_logger")?;
+    color_eyre::install().wrap_err("could not install color_eyre")?;
+
     if let Err(e) = dotenvy::dotenv() {
         log::warn!("Could not load .env file: {}", e);
     }
 
-    env_logger::try_init().wrap_err("could not initialize env_logger")?;
-    color_eyre::install()?;
     dkn_oracle::cli().await?;
 
     Ok(())
