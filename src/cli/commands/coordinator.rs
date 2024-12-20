b6ea162fc7726cbe801fc37c1ab6a96276db9da8
erhant <erhany96@gmail.com>
2024-12-20 10:26:44 +0300

    fix lints [skip ci]

--- src/cli/commands/coordinator.rs
+++ src/cli/commands/coordinator.rs
@@ -153,7 +153,7 @@ impl DriaOracle {
         );
 
         // handle request
-        match handle_request(self, &kinds, &model_config, event).await {
+        match handle_request(self, kinds, model_config, event).await {
             Ok(Some(receipt)) => {
                 log::info!(
                     "Task {} processed successfully. (tx: {})",
@@ -195,7 +195,7 @@ impl DriaOracle {
                 task_id,
                 log.transaction_hash.unwrap_or_default()
             );
-            match handle_request(self, &kinds, model_config, event).await {
+            match handle_request(self, kinds, model_config, event).await {
                 Ok(Some(receipt)) => {
                     log::info!(
                         "Task {} processed successfully. (tx: {})",
